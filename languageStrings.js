3970107df04b34db5dd2c6d3e2a93a474cb81ad7
Raquel <raquelpm23@gmail.com>
2020-04-09 13:38:15 +0200

    first initial skill

---
+++ languageStrings.js
@@ -0,0 +1,20 @@
+var languageStrings = {
+    "es": {
+        "translation": {
+            "FACTS": [
+                "Gus.",
+                "Cooper.",
+                "Rollo.",
+                "Greta.",
+                "Peanut"
+            ],
+            "SKILL_NAME" : "parque magico",
+            "GET_FACT_MESSAGE" : "tu personaje es: ",
+            "HELP_MESSAGE" : "You can say tell me a llama fact, or, you can say exit... What can I help you with?",
+            "HELP_REPROMPT" : "What can I help you with?",
+            "STOP_MESSAGE" : "hasta luego lucas!"
+        }
+    }
+};
+
+module.exports = languageStrings;
